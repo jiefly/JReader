058a4b6f94e5b3d48495d4ffce02ac570ccf5e72
jiefly <jiefly1993@gmail.com>
2016-07-19 23:40:18 +0800

    update ReaderView

--- readerview/src/main/java/com/gao/jiefly/readerview/ReaderView.java
+++ readerview/src/main/java/com/gao/jiefly/readerview/ReaderView.java
@@ -27,6 +27,8 @@ public class ReaderView extends View {
     private Bitmap prevBitmap;
     private Bitmap nextBitmap;
 
+    private static final String TAG = "jiefly";
+    boolean isPositionCenter = false;
     private int mWidth;
     private int mHeight;
 
@@ -57,10 +59,19 @@ public class ReaderView extends View {
     Path mPath1;
     Path mPath0;
 
+    int[] mBackShadowColors;
+    int[] mFrontShadowColors;
+
     GradientDrawable mBackShadowDrawableLR;
     GradientDrawable mBackShadowDrawableRL;
     GradientDrawable mFolderShadowDrawableLR;
     GradientDrawable mFolderShadowDrawableRL;
+
+    GradientDrawable mFrontShadowDrawableHBT;
+    GradientDrawable mFrontShadowDrawableHTB;
+    GradientDrawable mFrontShadowDrawableVLR;
+    GradientDrawable mFrontShadowDrawableVRL;
+
     private float mMaxLength;
 
 
@@ -74,20 +85,22 @@ public class ReaderView extends View {
         super(context);
         mWidth = screenSize.getWidth();
         mHeight = screenSize.getHeight();
+        mMaxLength = (float) Math.hypot(mWidth, mHeight);
         mPath0 = new Path();
         mPath1 = new Path();
-//        createDrawable();
+        createDrawable();
         // ---------------------------------------
-        mBitmap = Bitmap.createBitmap(480, 800, Bitmap.Config.ARGB_8888);
+        mBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
         mCanvas = new Canvas(mBitmap);
+        mCanvas.drawColor(0xFFAAAAAA);
+
         currentBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(currentBitmap);
         Paint paint = new Paint();
         canvas.drawColor(Color.YELLOW);
         canvas.drawBitmap(currentBitmap, 0, 0, paint);
 
-        nextBitmap = Bitmap
-                .createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
+        nextBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
         canvas = new Canvas(nextBitmap);
         canvas.drawColor(Color.GREEN);
         canvas.drawBitmap(nextBitmap, 0, 0, paint);
@@ -134,21 +147,17 @@ public class ReaderView extends View {
         mBezierControl2.y = mMiddleY - (mCornerX - mMiddleX)
                 * (mCornerX - mMiddleX) / (mCornerY - mMiddleY);
 
-        // Log.i("hmg", "mTouchX  " + mTouch.x + "  mTouchY  " + mTouch.y);
-        // Log.i("hmg", "mBezierControl1.x  " + mBezierControl1.x
-        // + "  mBezierControl1.y  " + mBezierControl1.y);
-        // Log.i("hmg", "mBezierControl2.x  " + mBezierControl2.x
-        // + "  mBezierControl2.y  " + mBezierControl2.y);
-
         mBezierStart1.x = mBezierControl1.x - (mCornerX - mBezierControl1.x) / 2;
         mBezierStart1.y = mCornerY;
 
-        // 当mBezierStart1.x < 0或者mBezierStart1.x > 480时
+        // 当mBezierStart1.x < 0或者mBezierStart1.x > mWidth时
         // 如果继续翻页，会出现BUG故在此限制
         if (mTouch.x > 0 && mTouch.x < mWidth) {
             if (mBezierStart1.x < 0 || mBezierStart1.x > mWidth) {
-                if (mBezierStart1.x < 0)
+                if (mBezierStart1.x < 0) {
+                    Log.e("<0", mBezierStart1 + "");
                     mBezierStart1.x = mWidth - mBezierStart1.x;
+                }
 
                 float f1 = Math.abs(mCornerX - mTouch.x);
                 float f2 = mWidth * f1 / mBezierStart1.x;
@@ -168,12 +177,6 @@ public class ReaderView extends View {
                 mBezierControl2.x = mCornerX;
                 mBezierControl2.y = mMiddleY - (mCornerX - mMiddleX)
                         * (mCornerX - mMiddleX) / (mCornerY - mMiddleY);
-                // Log.i("hmg", "mTouchX --> " + mTouch.x + "  mTouchY-->  "
-                // + mTouch.y);
-                // Log.i("hmg", "mBezierControl1.x--  " + mBezierControl1.x
-                // + "  mBezierControl1.y -- " + mBezierControl1.y);
-                // Log.i("hmg", "mBezierControl2.x -- " + mBezierControl2.x
-                // + "  mBezierControl2.y -- " + mBezierControl2.y);
                 mBezierStart1.x = mBezierControl1.x
                         - (mCornerX - mBezierControl1.x) / 2;
             }
@@ -190,11 +193,6 @@ public class ReaderView extends View {
         mBezierEnd2 = getCross(mTouch, mBezierControl2, mBezierStart1,
                 mBezierStart2);
 
-        // Log.i("hmg", "mBezierEnd1.x  " + mBezierEnd1.x + "  mBezierEnd1.y  "
-        // + mBezierEnd1.y);
-        // Log.i("hmg", "mBezierEnd2.x  " + mBezierEnd2.x + "  mBezierEnd2.y  "
-        // + mBezierEnd2.y);
-
 		/*
          * mBeziervertex1.x 推导
 		 * ((mBezierStart1.x+mBezierEnd1.x)/2+mBezierControl1.x)/2 化简等价于
@@ -204,6 +202,223 @@ public class ReaderView extends View {
         mBeziervertex1.y = (2 * mBezierControl1.y + mBezierStart1.y + mBezierEnd1.y) / 4;
         mBeziervertex2.x = (mBezierStart2.x + 2 * mBezierControl2.x + mBezierEnd2.x) / 4;
         mBeziervertex2.y = (2 * mBezierControl2.y + mBezierStart2.y + mBezierEnd2.y) / 4;
+
+    }
+
+
+    private void drawCurrentPageArea(Canvas canvas, Bitmap bitmap, Path path) {
+        mPath0.reset();
+        mPath0.moveTo(mBezierStart1.x, mBezierStart1.y);
+        mPath0.quadTo(mBezierControl1.x, mBezierControl1.y, mBezierEnd1.x,
+                mBezierEnd1.y);
+        mPath0.lineTo(mTouch.x, mTouch.y);
+        mPath0.lineTo(mBezierEnd2.x, mBezierEnd2.y);
+        mPath0.quadTo(mBezierControl2.x, mBezierControl2.y, mBezierStart2.x,
+                mBezierStart2.y);
+        mPath0.lineTo(mCornerX, mCornerY);
+        mPath0.close();
+        canvas.save();
+        canvas.clipPath(path, Region.Op.XOR);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        canvas.restore();
+    }
+
+    private void drawNextPageAreaAndShadow(Canvas canvas, Bitmap bitmap) {
+        int leftx;
+        int rightx;
+        GradientDrawable mBackShadowDrawable;
+        mPath1.reset();
+        mPath1.moveTo(mBezierStart1.x, mBezierStart1.y);
+        mPath1.lineTo(mBeziervertex1.x, mBeziervertex1.y);
+        mPath1.lineTo(mBeziervertex2.x, mBeziervertex2.y);
+        mPath1.lineTo(mBezierStart2.x, mBezierStart2.y);
+        mPath1.lineTo(mCornerX, mCornerY);
+        mPath1.close();
+
+        mDegrees = (float) Math.toDegrees(Math.atan2(mBezierControl1.x
+                - mCornerX, mBezierControl2.y - mCornerY));
+
+        if (mIsRTandLB) {
+            leftx = (int) (mBezierStart1.x);
+            rightx = (int) (mBezierStart1.x + mTouchToCornerDis / 4);
+            mBackShadowDrawable = mBackShadowDrawableLR;
+        } else {
+            leftx = (int) (mBezierStart1.x - mTouchToCornerDis / 4);
+            rightx = (int) mBezierStart1.x;
+            mBackShadowDrawable = mBackShadowDrawableRL;
+        }
+
+
+        Log.i("hmg", "leftx  " + leftx
+                + "   rightx  " + rightx);
+
+
+        canvas.save();
+        canvas.clipPath(mPath0);
+        canvas.clipPath(mPath1, Region.Op.INTERSECT);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        canvas.rotate(mDegrees, mBezierStart1.x, mBezierStart1.y);
+        mBackShadowDrawable.setBounds(leftx, (int) mBezierStart1.y, rightx,
+                (int) (mMaxLength + mBezierStart1.y));
+        Log.e("jiefly", "mMaxLength:" + mMaxLength);
+        mBackShadowDrawable.draw(canvas);
+        canvas.restore();
+    }
+
+    /**
+     * Author : hmg25 Version: 1.0 Description : 创建阴影的GradientDrawable
+     */
+    private void createDrawable() {
+        int[] color = {0x333333, 0xb0333333};
+        mFolderShadowDrawableRL = new GradientDrawable(
+                GradientDrawable.Orientation.RIGHT_LEFT, color);
+        mFolderShadowDrawableRL
+                .setGradientType(GradientDrawable.LINEAR_GRADIENT);
+//        mFolderShadowDrawableRL.setBounds(0,0,100,100);
+        mFolderShadowDrawableLR = new GradientDrawable(
+                GradientDrawable.Orientation.LEFT_RIGHT, color);
+        mFolderShadowDrawableLR
+                .setGradientType(GradientDrawable.LINEAR_GRADIENT);
+
+        mBackShadowColors = new int[]{0xff111111, 0x111111};
+        mBackShadowDrawableRL = new GradientDrawable(
+                GradientDrawable.Orientation.RIGHT_LEFT, mBackShadowColors);
+        mBackShadowDrawableRL.setGradientType(GradientDrawable.LINEAR_GRADIENT);
+
+        mBackShadowDrawableLR = new GradientDrawable(
+                GradientDrawable.Orientation.LEFT_RIGHT, mBackShadowColors);
+        mBackShadowDrawableLR.setGradientType(GradientDrawable.LINEAR_GRADIENT);
+
+        mFrontShadowColors = new int[]{0x80111111, 0x111111};
+        mFrontShadowDrawableVLR = new GradientDrawable(
+                GradientDrawable.Orientation.LEFT_RIGHT, mFrontShadowColors);
+        mFrontShadowDrawableVLR
+                .setGradientType(GradientDrawable.LINEAR_GRADIENT);
+        mFrontShadowDrawableVRL = new GradientDrawable(
+                GradientDrawable.Orientation.RIGHT_LEFT, mFrontShadowColors);
+        mFrontShadowDrawableVRL
+                .setGradientType(GradientDrawable.LINEAR_GRADIENT);
+
+        mFrontShadowDrawableHTB = new GradientDrawable(
+                GradientDrawable.Orientation.TOP_BOTTOM, mFrontShadowColors);
+        mFrontShadowDrawableHTB
+                .setGradientType(GradientDrawable.LINEAR_GRADIENT);
+
+        mFrontShadowDrawableHBT = new GradientDrawable(
+                GradientDrawable.Orientation.BOTTOM_TOP, mFrontShadowColors);
+        mFrontShadowDrawableHBT
+                .setGradientType(GradientDrawable.LINEAR_GRADIENT);
+    }
+
+    /**
+     * Author : hmg25 Version: 1.0 Description : 绘制翻起页的阴影
+     */
+    public void drawCurrentPageShadow(Canvas canvas) {
+        double degree;
+        if (mIsRTandLB) {
+            degree = Math.PI
+                    / 4
+                    - Math.atan2(mBezierControl1.y - mTouch.y, mTouch.x - mBezierControl1.x);
+        } else {
+            degree = Math.PI
+                    / 4
+                    - Math.atan2(mTouch.y - mBezierControl1.y, mTouch.x - mBezierControl1.x);
+        }
+        // 翻起页阴影顶点与touch点的距离
+        double d1 = (float) 35 * 1.414 * Math.cos(degree);
+        double d2 = (float) 35 * 1.414 * Math.sin(degree);
+        float x = (float) (mTouch.x + d1);
+        float y;
+
+        if (mIsRTandLB) {
+            y = (float) (mTouch.y + d2);
+        } else {
+            y = (float) (mTouch.y - d2);
+        }
+        mPath1.reset();
+        mPath1.moveTo(x, y);
+        mPath1.lineTo(mTouch.x, mTouch.y);
+        mPath1.lineTo(mBezierControl1.x, mBezierControl1.y);
+        mPath1.lineTo(mBezierStart1.x, mBezierStart1.y);
+        mPath1.close();
+        float rotateDegrees;
+        canvas.save();
+
+        canvas.clipPath(mPath0, Region.Op.XOR);
+        canvas.clipPath(mPath1, Region.Op.INTERSECT);
+        int leftx;
+        int rightx;
+        GradientDrawable mCurrentPageShadow;
+        if (mIsRTandLB) {
+            leftx = (int) (mBezierControl1.x);
+            rightx = (int) mBezierControl1.x + 25;
+            mCurrentPageShadow = mFrontShadowDrawableVLR;
+        } else {
+            leftx = (int) (mBezierControl1.x - 25);
+            rightx = (int) mBezierControl1.x + 1;
+            mCurrentPageShadow = mFrontShadowDrawableVRL;
+        }
+
+        rotateDegrees = (float) Math.toDegrees(Math.atan2(mTouch.x
+                - mBezierControl1.x, mBezierControl1.y - mTouch.y));
+        canvas.rotate(rotateDegrees, mBezierControl1.x, mBezierControl1.y);
+        mCurrentPageShadow.setBounds(leftx,
+                (int) (mBezierControl1.y - mMaxLength), rightx,
+                (int) (mBezierControl1.y));
+        mCurrentPageShadow.draw(canvas);
+        canvas.restore();
+
+        mPath1.reset();
+        mPath1.moveTo(x, y);
+        mPath1.lineTo(mTouch.x, mTouch.y);
+        mPath1.lineTo(mBezierControl2.x, mBezierControl2.y);
+        mPath1.lineTo(mBezierStart2.x, mBezierStart2.y);
+        mPath1.close();
+        canvas.save();
+        canvas.clipPath(mPath0, Region.Op.XOR);
+        canvas.clipPath(mPath1, Region.Op.INTERSECT);
+        if (mIsRTandLB) {
+            leftx = (int) (mBezierControl2.y);
+            rightx = (int) (mBezierControl2.y + 25);
+            mCurrentPageShadow = mFrontShadowDrawableHTB;
+        } else {
+            leftx = (int) (mBezierControl2.y - 25);
+            rightx = (int) (mBezierControl2.y + 1);
+            mCurrentPageShadow = mFrontShadowDrawableHBT;
+        }
+        rotateDegrees = (float) Math.toDegrees(Math.atan2(mBezierControl2.y
+                - mTouch.y, mBezierControl2.x - mTouch.x));
+        canvas.rotate(rotateDegrees, mBezierControl2.x, mBezierControl2.y);
+        float temp;
+        if (mBezierControl2.y < 0)
+            temp = mBezierControl2.y - mHeight;
+        else
+            temp = mBezierControl2.y;
+
+        int hmg = (int) Math.hypot(mBezierControl2.x, temp);
+        if (hmg > mMaxLength)
+            mCurrentPageShadow
+                    .setBounds((int) (mBezierControl2.x - 25) - hmg, leftx,
+                            (int) (mBezierControl2.x + mMaxLength) - hmg,
+                            rightx);
+        else
+            mCurrentPageShadow.setBounds(
+                    (int) (mBezierControl2.x - mMaxLength), leftx,
+                    (int) (mBezierControl2.x), rightx);
+
+        // Log.i("hmg", "mBezierControl2.x   " + mBezierControl2.x
+        // + "  mBezierControl2.y  " + mBezierControl2.y);
+        mCurrentPageShadow.draw(canvas);
+        canvas.restore();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(0xFFAAAAAA);
+        calcPoints();
+        drawCurrentPageArea(canvas, currentBitmap, mPath0);
+        drawNextPageAreaAndShadow(canvas, nextBitmap);
+        drawCurrentPageShadow(canvas);
     }
 
     /**
@@ -234,102 +449,46 @@ public class ReaderView extends View {
             mCornerY = 0;
         else
             mCornerY = mHeight;
-        if ((mCornerX == 0 && mCornerY == mHeight)
-                || (mCornerX == mWidth && mCornerY == 0))
-            mIsRTandLB = true;
-        else
-            mIsRTandLB = false;
-    }
 
+        mIsRTandLB = (mCornerX == 0 && mCornerY == mHeight) || (mCornerX == mWidth && mCornerY == 0);
 
-    private void drawCurrentPageArea(Canvas canvas, Bitmap bitmap, Path path) {
-        mPath0.reset();
-        mPath0.moveTo(mBezierStart1.x, mBezierStart1.y);
-        mPath0.quadTo(mBezierControl1.x, mBezierControl1.y, mBezierEnd1.x,
-                mBezierEnd1.y);
-        mPath0.lineTo(mTouch.x, mTouch.y);
-        mPath0.lineTo(mBezierEnd2.x, mBezierEnd2.y);
-        mPath0.quadTo(mBezierControl2.x, mBezierControl2.y, mBezierStart2.x,
-                mBezierStart2.y);
-        mPath0.lineTo(mCornerX, mCornerY);
-        mPath0.close();
-
-        canvas.save();
-        canvas.clipPath(path, Region.Op.XOR);
-        canvas.drawBitmap(bitmap, 0, 0, null);
-        canvas.restore();
-    }
-
-    private void drawNextPageAreaAndShadow(Canvas canvas, Bitmap bitmap) {
-        mPath1.reset();
-        mPath1.moveTo(mBezierStart1.x, mBezierStart1.y);
-        mPath1.lineTo(mBeziervertex1.x, mBeziervertex1.y);
-        mPath1.lineTo(mBeziervertex2.x, mBeziervertex2.y);
-        mPath1.lineTo(mBezierStart2.x, mBezierStart2.y);
-        mPath1.lineTo(mCornerX, mCornerY);
-        mPath1.close();
-
-        mDegrees = (float) Math.toDegrees(Math.atan2(mBezierControl1.x
-                - mCornerX, mBezierControl2.y - mCornerY));
-        float f5 = mTouchToCornerDis / 4;
-        int leftx;
-        int rightx;
-        GradientDrawable mBackShadowDrawable;
-        if (mIsRTandLB) {
-            leftx = (int) (mBezierStart1.x - 1);
-            rightx = (int) (mBezierStart1.x + mTouchToCornerDis / 4 + 1);
-            mBackShadowDrawable = mBackShadowDrawableLR;
-        } else {
-            leftx = (int) (mBezierStart1.x - mTouchToCornerDis / 4 - 1);
-            rightx = (int) mBezierStart1.x + 1;
-            mBackShadowDrawable = mBackShadowDrawableRL;
-        }
-
-        Log.i("hmg", "leftx  " + leftx
-                + "   rightx  " + rightx);
-        canvas.save();
-        canvas.clipPath(mPath0);
-        canvas.clipPath(mPath1, Region.Op.INTERSECT);
-        canvas.drawBitmap(bitmap, 0, 0, null);
-        canvas.rotate(mDegrees, mBezierStart1.x, mBezierStart1.y);
-//        mBackShadowDrawable.setBounds(leftx,
-//                (int) mBezierStart1.y, rightx,
-//                (int) (mMaxLength + mBezierStart1.y));
-//        mBackShadowDrawable.draw(canvas);
-        canvas.restore();
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawColor(0xFFAAAAAA);
-        calcPoints();
-        drawCurrentPageArea(mCanvas, currentBitmap, mPath0);
-        drawNextPageAreaAndShadow(mCanvas, nextBitmap);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         // TODO Auto-generated method stub
-        if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            mCanvas.drawColor(0xFFAAAAAA);
-            mTouch.x = event.getX();
-            mTouch.y = event.getY();
-            this.postInvalidate();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mCanvas.drawColor(0xFFAAAAAA);
+                Log.e("position", event.getX() + "--=-=--" + event.getY());
+                if (event.getY() > mHeight / 3 && event.getY() < mHeight * 2 / 3) {
+                    isPositionCenter = true;
+                    mTouch.y = mHeight - 0.01f;
+                } else
+                    mTouch.y = event.getY();
+                mTouch.x = event.getX();
+                calcCornerXY(mTouch.x, mTouch.y);
+                Log.e("jjiefluy", "+" + isPositionCenter);
+                this.postInvalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mCanvas.drawColor(0xFFAAAAAA);
+                mTouch.x = event.getX();
+                if (isPositionCenter)
+                    mTouch.y = mHeight - 0.01f;
+                else
+                    mTouch.y = event.getY();
+                this.postInvalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                mCanvas.drawColor(0xFFAAAAAA);
+                mTouch.x = mCornerX;
+                mTouch.y = mCornerY;
+                isPositionCenter = false;
+                Log.e("jjiefluy", "up" + isPositionCenter);
+                this.postInvalidate();
+                break;
         }
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            mCanvas.drawColor(0xFFAAAAAA);
-            mTouch.x = event.getX();
-            mTouch.y = event.getY();
-            calcCornerXY(mTouch.x, mTouch.y);
-            this.postInvalidate();
-        }
-        if (event.getAction() == MotionEvent.ACTION_UP) {
-            mCanvas.drawColor(0xFFAAAAAA);
-            mTouch.x = mCornerX;
-            mTouch.y = mCornerY;
-            this.postInvalidate();
-        }
-        // return super.onTouchEvent(event);
         return true;
     }
 }
